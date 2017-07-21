f99a6848d446ccfb5949b88a6c41ce8b9c9d210e
araki <araki@qoncept.co.jp>
2017-07-21 20:48:03 +0900

    Loop unroll

--- Sources/Cxorswift/xorswift.cpp
+++ Sources/Cxorswift/xorswift.cpp
@@ -18,8 +18,7 @@ void xorshift(unsigned int *start, int count) {
         t1 = x ^ (x << 11);
         x = y; y = z; z = w;
         w = (w ^ (w >> 19)) ^ (t1 ^ (t1 >> 8));
-        *p = w;
-        ++p;
+        *(p++) = w;
     }
     
     for(; i < count ; i += 4) {
@@ -39,37 +38,70 @@ void xorshift(unsigned int *start, int count) {
 }
 
 void xorshift_uniform(float *start, int count, float low, float high) {
-    uint32_t t;
+    uint32_t t1, t2, t3, t4;
     float *p = start; // in [0, 1)
     float divisor = nextafterf(UINT32_MAX, UINT64_MAX);
+    int i;
     
-    for(int i = 0 ; i < count ; ++i) {
-        t = x ^ (x << 11);
+    for(i = 0 ; i < count%4 ; ++i) {
+        t1 = x ^ (x << 11);
         x = y; y = z; z = w;
-        w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
-        *p = (high - low) * (float)w / divisor + low;
-        ++p;
+        w = (w ^ (w >> 19)) ^ (t1 ^ (t1 >> 8));
+        *(p++) = (high - low) * (float)w / divisor + low;
+    }
+    
+    for(; i < count ; i += 4) {
+        t1 = x ^ (x << 11);
+        t2 = y ^ (y << 11);
+        t3 = z ^ (z << 11);
+        t4 = w ^ (w << 11);
+        x = (w ^ (w >> 19)) ^ (t1 ^ (t1 >> 8));
+        y = (x ^ (x >> 19)) ^ (t2 ^ (t2 >> 8));
+        z = (y ^ (y >> 19)) ^ (t3 ^ (t3 >> 8));
+        w = (z ^ (z >> 19)) ^ (t4 ^ (t4 >> 8));
+        *(p++) = (high - low) * (float)x / divisor + low;
+        *(p++) = (high - low) * (float)y / divisor + low;
+        *(p++) = (high - low) * (float)z / divisor + low;
+        *(p++) = (high - low) * (float)w / divisor + low;
     }
 }
 
 void _xorshift_normal(float *start, int count, float mu, float sigma) {
-    uint32_t t;
+    uint32_t t1, t2, t3, t4;
     float *p = start; // ~ N(mu, sigma)
     float x1, x2; // in (0, 1)
     float divisor = nextafterf(UINT32_MAX, UINT64_MAX);
     
-    for(int i = 0 ; i < count ; ++i) {
-        t = x ^ (x << 11);
+    if(count%2 == 1) {
+        t1 = x ^ (x << 11);
         x = y; y = z; z = w;
-        w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+        w = (w ^ (w >> 19)) ^ (t1 ^ (t1 >> 8));
         x1 = (float)w / divisor + FLT_MIN;
         
-        t = x ^ (x << 11);
+        t1 = x ^ (x << 11);
         x = y; y = z; z = w;
-        w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+        w = (w ^ (w >> 19)) ^ (t1 ^ (t1 >> 8));
         x2 = (float)w / divisor + FLT_MIN;
         
-        *p = sigma * sqrtf(-2*logf(x1)) * cosf(2*M_PI*x2) + mu;
-        ++p;
+        *(p++) = sigma * sqrtf(-2*logf(x1)) * cosf(2*M_PI*x2) + mu;
+    }
+    
+    for(int i = 0 ; i < count ; i+=2) {
+        t1 = x ^ (x << 11);
+        t2 = y ^ (y << 11);
+        t3 = z ^ (z << 11);
+        t4 = w ^ (w << 11);
+        x = (w ^ (w >> 19)) ^ (t1 ^ (t1 >> 8));
+        y = (x ^ (x >> 19)) ^ (t2 ^ (t2 >> 8));
+        z = (y ^ (y >> 19)) ^ (t3 ^ (t3 >> 8));
+        w = (z ^ (z >> 19)) ^ (t4 ^ (t4 >> 8));
+        
+        x1 = (float)x / divisor + FLT_MIN;
+        x2 = (float)y / divisor + FLT_MIN;
+        *(p++) = sigma * sqrtf(-2*logf(x1)) * cosf(2*M_PI*x2) + mu;
+        
+        x1 = (float)z / divisor + FLT_MIN;
+        x2 = (float)w / divisor + FLT_MIN;
+        *(p++) = sigma * sqrtf(-2*logf(x1)) * cosf(2*M_PI*x2) + mu;
     }
 }
