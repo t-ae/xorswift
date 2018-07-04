b3a68f640bb31d54bbf15db08aa4c7fe926c6ba9
araki <araki@qoncept.co.jp>
2018-07-04 17:11:53 +0900

    Add test

--- Tests/XorswiftTests/OtherTests.swift
+++ Tests/XorswiftTests/OtherTests.swift
@@ -2,15 +2,26 @@ import XCTest
 import Accelerate
 
 class OtherTests: XCTestCase {
+    func testFloatMake() {
+        let zero: UInt32 = 0
+        let maxx: UInt32 = UInt32.max
+        
+        let minimum = Float(bitPattern: zero >> 9 | 0x3f80_0000)
+        XCTAssertEqual(minimum, 1)
+        
+        let maximum = Float(bitPattern: maxx >> 9 | 0x3f80_0000)
+        XCTAssertEqual(maximum, nextafter(2, 0))
+    }
+    
     func testDoubleMake() {
         let zero: UInt32 = 0
         let maxx: UInt32 = UInt32.max
         
-        let minimum = Double(bitPattern: UInt64(zero<<12)<<20 | UInt64(zero) | 0x3ff0_0000_0000_0000) - 1
-        XCTAssertEqual(minimum, 0)
+        let minimum = Double(bitPattern: UInt64(zero<<12)<<20 | UInt64(zero) | 0x3ff0_0000_0000_0000)
+        XCTAssertEqual(minimum, 1)
         
-        let maximum = Double(bitPattern: UInt64(maxx<<12)<<20 | UInt64(maxx) | 0x3ff0_0000_0000_0000) - 1
-        XCTAssertLessThan(maximum, 1)
+        let maximum = Double(bitPattern: UInt64(maxx<<12)<<20 | UInt64(maxx) | 0x3ff0_0000_0000_0000)
+        XCTAssertEqual(maximum, nextafter(2, 0))
     }
     
     func testShiftOperation() {
